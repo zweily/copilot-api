917dfa13a509abdd0b8e73bbf00058c2217160ee
Erick Christian <erickchristian48@gmail.com>
2025-01-14 00:15:24 +0700

    feat: [wip] change to oauth

--- src/services/copilot-vscode/get-token/github-token.ts
+++ src/services/copilot-vscode/get-token/github-token.ts
@@ -1,60 +1,59 @@
-import { execa } from "execa"
+import consola from "consola"
 
-// @ts-expect-error TypeScript can't analyze timeout
-export async function getGitHubToken(): Promise<string> {
-  // Kill any existing vscode processes
-  // otherwise, no token call will be made
-  await killVSCodeProcesses()
+import { ENV } from "~/lib/env"
+import { _github } from "~/services/api-instance"
 
-  const mitmdump = createMitmdumpProcess()
-  void createVSCodeProcess()
-
-  const timeout = setTimeout(() => {
-    throw new Error("Timed out waiting for token")
-  }, 30_000)
-
-  for await (const line of mitmdump.stdout) {
-    if (typeof line !== "string") continue
-    if (!line.includes("authorization: token")) continue
-
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const token = line
-      .split("\n")
-      .map((line) => line.trim())
-      .filter(Boolean)
-      .find((line) => line.includes("authorization: token"))!
-      .split("authorization: token ")
-      .at(1)!
-      .trim()
-
-    clearTimeout(timeout)
-
-    await killVSCodeProcesses()
-    mitmdump.kill()
-
-    return token
-  }
+interface DeviceCodeResponse {
+  device_code: string
+  user_code: string
+  verification_uri: string
+  expires_in: number
+  interval: number
 }
 
-const createMitmdumpProcess = () =>
-  execa({ reject: false })("mitmdump", [
-    "--flow-detail",
-    "4",
-    "~m GET & ~u https://api.github.com/copilot_internal/v2/token",
-  ])
+interface AccessTokenResponse {
+  access_token: string
+  token_type: string
+  scope: string
+}
 
-const createVSCodeProcess = () =>
-  execa({
-    reject: false,
-    env: {
-      http_proxy: "http://127.0.0.1:8080",
-      https_proxy: "http://127.0.0.1:8080",
+export async function getGitHubToken(): Promise<string> {
+  consola.start("Getting GitHub device code")
+
+  const response = await _github<DeviceCodeResponse>("/login/device/code", {
+    method: "POST",
+    body: {
+      client_id: ENV.GITHUB_CLIENT_ID,
+      scope: ENV.GITHUB_OAUTH_SCOPES,
     },
-  })("code", [
-    "--ignore-certificate-errors",
-    // Can be whatever folder, as long as it's trusted by vscode
-    // https://code.visualstudio.com/docs/editor/workspace-trust
-    "/home/erick/Documents/sides/playground/",
-  ])
+  })
 
-const killVSCodeProcesses = () => execa({ reject: false })("pkill", ["code"])
+  consola.info(
+    `Please enter the code "${response.user_code}" in ${response.verification_uri}`,
+  )
+
+  while (true) {
+    const pollResponse = await _github<AccessTokenResponse>(
+      "/login/oauth/access_token",
+      {
+        method: "POST",
+        body: {
+          client_id: ENV.GITHUB_CLIENT_ID,
+          device_code: response.device_code,
+          grant_type: "urn:ietf:params:oauth:grant-type:device_code",
+        },
+      },
+    )
+
+    if (pollResponse.access_token) {
+      consola.info(`Got token ${pollResponse.access_token.replace(/./g, "*")}`)
+      return pollResponse.access_token
+    } else {
+      // Interval is in seconds, we need to multiply by 1000 to get milliseconds
+      // I'm also adding another second, just to be safe
+      await new Promise((resolve) =>
+        setTimeout(resolve, (response.interval + 1) * 1000),
+      )
+    }
+  }
+}
