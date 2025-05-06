9c6b7b9ecc22a6d7fbe7c6a35a1e6fcc439c47ea
Erick Christian <erickchristian48@gmail.com>
2025-05-07 00:19:35 +0700

    chore: remove console.log

--- src/lib/tokenizer.ts
+++ src/lib/tokenizer.ts
@@ -6,7 +6,6 @@ export const getTokenCount = (messages: Array<Message>) => {
   const input = messages.filter(
     (m) => m.role !== "assistant" && typeof m.content === "string",
   )
-  console.log(input)
   const output = messages.filter((m) => m.role === "assistant")
 
   const inputTokens = countTokens(input)
