41af4a7b2a13c261c1a572d46a3813996509f1ce
Filip Trplan <filip@trplan.si>
2025-05-06 18:19:23 +0200

    fix: prevent countTokens from counting images

--- src/lib/tokenizer.ts
+++ src/lib/tokenizer.ts
@@ -3,7 +3,10 @@ import { countTokens } from "gpt-tokenizer/model/gpt-4o"
 import type { Message } from "~/services/copilot/create-chat-completions"
 
 export const getTokenCount = (messages: Array<Message>) => {
-  const input = messages.filter((m) => m.role !== "assistant")
+  const input = messages.filter(
+    (m) => m.role !== "assistant" && typeof m.content === "string",
+  )
+  console.log(input)
   const output = messages.filter((m) => m.role === "assistant")
 
   const inputTokens = countTokens(input)
