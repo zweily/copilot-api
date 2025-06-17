544d0a3ca5a1e977fd584585b2d6619f387d47cc
Erick Christian <erickchristian48@gmail.com>
2025-06-17 14:48:32 +0700

    feat: 5 seconds timeout on get vscode ver

--- src/services/get-vscode-version.ts
+++ src/services/get-vscode-version.ts
@@ -1,19 +1,31 @@
 const FALLBACK = "1.98.1"
 
 export async function getVSCodeVersion() {
-  const response = await fetch(
-    "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=visual-studio-code-bin",
-  )
+  const controller = new AbortController()
+  const timeout = setTimeout(() => {
+    controller.abort()
+  }, 5000)
 
-  const pkgbuild = await response.text()
-  const pkgverRegex = /pkgver=([0-9.]+)/
-  const match = pkgbuild.match(pkgverRegex)
+  try {
+    const response = await fetch(
+      "https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=visual-studio-code-bin",
+      { signal: controller.signal },
+    )
 
-  if (match) {
-    return match[1]
+    const pkgbuild = await response.text()
+    const pkgverRegex = /pkgver=([0-9.]+)/
+    const match = pkgbuild.match(pkgverRegex)
+
+    if (match) {
+      return match[1]
+    }
+
+    return FALLBACK
+  } catch {
+    return FALLBACK
+  } finally {
+    clearTimeout(timeout)
   }
-
-  return FALLBACK
 }
 
 await getVSCodeVersion()
